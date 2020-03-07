ee3c1f7dfb155e94dda57417b6f09638ccdac0d7
Raclarke5179 <raclarke9834@gmail.com>
2020-03-07 13:05:33 -0600

    Initial Commit
    Signed-off-by: Raclarke5179 <raclarke9834@gmail.com>

---
+++ NewTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  
+	  System.out.println("This is TestNG-Maven Example");
+  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
